8a308f53283bcde15ef05f69e3af8395a8752652
George Shaw <george.e.shaw.iv@outlook.com>
2017-09-05 17:07:25 -0400

    released sites and vendor cache and updated home_page.js

--- themes/custom/bethelks/js/home_page.js
+++ themes/custom/bethelks/js/home_page.js
@@ -145,7 +145,7 @@ jQuery(document).on('ready', function(){
   jQuery('.js-location-map').each(function(index){
     var active = false;
     var imgDiv = jQuery(this).find('img').parent().parent().parent();
-    //imgDiv.html('<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3140.7915447772607!2d-97.34600988455563!3d38.07523550258839!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87bb135af58a1f63%3A0xca6979b523a4e897!2sBethel+College!5e0!3m2!1sen!2sus!4v1504209031579" width="100%" height="400px" frameborder="0" style="border:0" allowfullscreen></iframe>');
+    imgDiv.html('<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3140.7915447772607!2d-97.34600988455563!3d38.07523550258839!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87bb135af58a1f63%3A0xca6979b523a4e897!2sBethel+College!5e0!3m2!1sen!2sus!4v1504209031579" width="100%" height="400px" frameborder="0" style="border:0" allowfullscreen></iframe>');
 
     imgDiv.hide();
     jQuery(this).find('h2').parent().append('<i class="fa fa-angle-down"></i>');
