9db663839a852d6b1b2a895c901d3a1dc7e9280e
George Shaw <george.e.shaw.iv@outlook.com>
2018-04-13 14:30:32 -0500

    wowo

--- modules/bethelks_staff/src/Breadcrumb/bethelksStaffBreadcrumbBuilder.php
+++ modules/bethelks_staff/src/Breadcrumb/bethelksStaffBreadcrumbBuilder.php
@@ -7,24 +7,24 @@ use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Link;
 
 class bethelksStaffBreadcrumbBuilder implements BreadcrumbBuilderInterface {
-  // public function applies(RouteMatchInterface $attributes) {
-  //   $parameters = $attributes->getParameters()->all();
-  //
-  //   if(isset($parameters['singleStaff']) && $parameters['singleStaff'] == true) {
-  //     return true;
-  //   }
-  //   else {
-  //     return false;
-  //   }
-  // }
-  //
-  // public function build(RouteMatchInterface $route_match) {
-  //   $breadcrumb = new Breadcrumb();
-  //
-  //   $breadcrumb->addLink(Link::createFromRoute('Home', '<front>'));
-  //   $breadcrumb->addLink(Link::createFromRoute('Employee', 'bethelks_staff.faculty'));
-  //
-  //   return $breadcrumb;
-  // }
+  public function applies(RouteMatchInterface $attributes) {
+    // $parameters = $attributes->getParameters()->all();
+    //
+    // if(isset($parameters['singleStaff']) && $parameters['singleStaff'] == true) {
+    //   return true;
+    // }
+    // else {
+    //   return false;
+    // }
+  }
+
+  public function build(RouteMatchInterface $route_match) {
+    // $breadcrumb = new Breadcrumb();
+    //
+    // $breadcrumb->addLink(Link::createFromRoute('Home', '<front>'));
+    // $breadcrumb->addLink(Link::createFromRoute('Employee', 'bethelks_staff.faculty'));
+    //
+    // return $breadcrumb;
+  }
 }
 ?>
